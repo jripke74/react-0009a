dbdc24a92498e74e4af74d9f56895b5fdddd02cf
Jeff Ripke <jripke74@gmail.com>
2022-08-29 15:12:33 -0700

    add Expenses and UI folders

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './ExpenseDate.css'
+
+import './ExpenseDate.css';
 
 function ExpenseDate(props) {
   const month = props.date.toLocaleString('en-US', { month: 'long' });
@@ -7,10 +8,10 @@ function ExpenseDate(props) {
   const year = props.date.getFullYear();
 
   return (
-    <div className='expense-date'>
-      <div className='expense-date__month'>{month}</div>
-      <div className='expense-date__year'>{year}</div>
-      <div className='expense-date__day'>{day}</div>
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   );
 }
